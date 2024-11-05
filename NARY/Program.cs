982faad3b1a85a22ee27637b41c91d72e771b812
Дарина <yashina.dd2778@okeit.edu>
2024-11-05 11:00:32 +0500

    check

---
+++ NARY/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NARY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 14562;
+            int c = 238;
+            Console.WriteLine(a);
+
+        }
+    }
+}
